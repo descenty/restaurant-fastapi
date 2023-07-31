b9794449a5184c43b38877ee0a37e510ba26bc2f
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-07-31 09:54:29 +0300

    change config

--- core/config.py
+++ core/config.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -9,10 +9,6 @@ class Settings(BaseSettings):
         "postgresql+asyncpg://postgres:qweasdzxc@localhost:5432/restaurant"
     )
     cors_allow_origins: List[str] = ["http://localhost"]
-
-    # model_config = SettingsConfigDict(
-    #     env_file=".env", env_file_encoding="utf-8"
-    # )
 
 
 settings = Settings()
