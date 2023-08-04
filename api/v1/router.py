d2acdcfa8390f0c11ffe7f3f34a8caf7889e7141
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-08-04 15:03:11 +0300

    uncomment routes

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
 
-from .endpoints.menus import router as menus_router
-
-# from .endpoints.submenus import SubmenuController
-# from .endpoints.dishes import DishController
+from .endpoints import menus
+from .endpoints import submenus
+from .endpoints import dishes
 
 
 api_router = APIRouter()
-api_router.include_router(menus_router, prefix="/menus", tags=["menus"])
-# api_router.include_router(submenus.router, prefix="/menus", tags=["submenus"])
-# api_router.include_router(dishes.router, prefix="/menus", tags=["dishes"])
+api_router.include_router(menus.router, prefix="/menus", tags=["menus"])
+api_router.include_router(submenus.router, prefix="/menus", tags=["submenus"])
+api_router.include_router(dishes.router, prefix="/menus", tags=["dishes"])
